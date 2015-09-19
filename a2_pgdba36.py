b493398f5ca85de66a8d01af2cfecc9eb0858ff9
robin93 <robinsinghiitd@gmail.com>
2015-09-19 15:09:29 +0530

    added Binary Search

--- a2_pgdba36.py
+++ a2_pgdba36.py
@@ -16,10 +16,7 @@ def clean_list(input_list):
 	user_input_list = []
 	for i in input_list:
 		if not i.isalpha():
-			if float(i)%1>0:
-				user_input_list.append(float(i))
-			else:
-				user_input_list.append(int(i))
+			user_input_list.append(float(i))
 	return user_input_list
 
 def input_for_Linear_search():
@@ -38,14 +35,18 @@ def input_number_for_search():
 			continue
 		else:
 			break
-	if float(search_number)%1>0:
-		return float(search_number)
-	else:
-		return int(search_number)
+	return float(search_number)
 	
 def input_for_sorting():
 	numbers = raw_input("Enter a comma seperated list of numbers to be sorted :").split(",")
 	return clean_list(numbers)
+
+def mid_element_index(input_list):
+		size = len(input_list)
+		if size%2 == 0:
+			return ((size/2)-1)
+		else:
+			return ((size+1)/2)-1
 
 def linear_search(input_list,input_number):
 	steps = 0	
@@ -57,12 +58,35 @@ def linear_search(input_list,input_number):
 	
 	#print output to console
 	if steps == len(input_list):
-		print "Number is not an element of the list"
+		print "\nNumber is not an element of the list"
 	else:
-		print "Number found in the list at %d position in %d number of steps"%steps,%steps
+		print "\n\nNumber found in the list at %d position in %d number of steps"%(steps,steps) 
 
 def binary_search(input_list,input_number):
-	print input_list ,input_number
+	
+	#inplementing the terminating condition for the binary search
+	if len(input_list) == 1:
+		if input_list[0] == input_number:
+			print "Number found in the list"	
+			return
+		else:
+			print "Number not found in the list"
+			return
+	#if the terminating condition is false then the function goes into recursion
+	else: 
+		mid_index = mid_element_index(input_list)
+		mid_val = input_list[mid_index]
+		print "The number in this step is compared to: ",mid_val
+
+		#recursive part of the function
+		if input_number == mid_val:
+			print "Number found in the list in list"
+		elif input_number > mid_val:
+			binary_search(input_list[mid_index+1:len(input_list)],input_number)
+		else:
+			binary_search(input_list[0:mid_index+1],input_number)
+
+
 def insertion_sort(input_list):
 	print input_list
 def selection_sort(input_list):
